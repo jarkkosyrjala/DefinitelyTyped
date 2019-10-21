ce3875ed2e548cc9388a888b4a0c619cdd8b3f54
Jarkko Syrjälä <j@rkko.net>
2019-10-21 13:53:04 +0300

    fix(node): fix ParsedUrlQueryInput typing
    The value can be anything while only string | number | boolean | string[] | number[] | boolean[] are parsed to the resulting string according to Node.js documentation: https://nodejs.org/api/querystring.html#querystring_querystring_stringify_obj_sep_eq_options

--- types/node/querystring.d.ts
+++ types/node/querystring.d.ts
@@ -10,12 +10,7 @@ declare module "querystring" {
 
     interface ParsedUrlQuery { [key: string]: string | string[]; }
 
-    interface ParsedUrlQueryInput {
-        [key: string]:
-        // The value type here is a "poor man's `unknown`". When these types support TypeScript
-        // 3.0+, we can replace this with `unknown`.
-        {} | null | undefined;
-    }
+    interface ParsedUrlQueryInput { [key: string]: any; }
 
     function stringify(obj?: ParsedUrlQueryInput, sep?: string, eq?: string, options?: StringifyOptions): string;
     function parse(str: string, sep?: string, eq?: string, options?: ParseOptions): ParsedUrlQuery;
